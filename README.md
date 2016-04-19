8086e99d0a4d66945466d46d6b9def5e0a7fcd16
蔺仕峰 <linsf@gcks.cn>
2016-04-19 14:54:24 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # multipleselect
+
+左右选择的多选框
+
+ <script>
+ $().ready(function(){
+
+ $('.ovomultiselect').ovoMultipleSelect({'fromDataUrl':FaceConf.apiurl + '/ipc/findipc'});
+ $('.ovomultiselect').ovoMultipleSelect({fromData: [{name: '1111', value: '1111'}, {name: '222', value: '222'}, {name: '333',value: '333'}, {name: '444', value: '444'}, {name: '555', value: '555'}, {name: '666', value: '666'}]});
+
+ });
+ </script>
--- src/multipleselect.js
+++ src/multipleselect.js
@@ -1,19 +1,8 @@
 /*
- * multipleselect 1.0.0
+ * multipleselect 0.1.0
  * Copyright (c) 2016 OvO
  * Date: 2016-04-06
  * 多选框
- *
- *
- *
- <script>
- $().ready(function(){
-
- $('.ovomultiselect').ovoMultipleSelect({'fromDataUrl':FaceConf.apiurl + '/ipc/findipc'});
- $('.ovomultiselect').ovoMultipleSelect({fromData: [{name: '1111', value: '1111'}, {name: '222', value: '222'}, {name: '333',value: '333'}, {name: '444', value: '444'}, {name: '555', value: '555'}, {name: '666', value: '666'}]});
-
- });
- </script>
  *
  */
 
@@ -37,7 +26,7 @@
         /**
          * @name $.ovoMultipleSelect.version
          */
-        version: '1.0.0',
+        version: '0.1.0',
         author: "OvO",
         /**
          * holds all the default options used when creating new instances
